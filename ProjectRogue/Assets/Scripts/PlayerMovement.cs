5f476ff4f3ee56db9d3214ac7b69bd467951a0bb
mitchellcurrie <mitchell.jc@hotmail.com>
2017-11-25 21:24:37 +1300

    Add third person camera controls

--- ProjectRogue/Assets/Scripts/PlayerMovement.cs
+++ ProjectRogue/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    private Camera cam;
+
     public float speed;
     private Vector3 velocity;
-    
-    
+    private Vector3 moveDirection;
+
+
     // Use this for initialization
-	void Start()
+    void Start()
     {
-      
+        cam = Camera.main;
     }
 	
 	// Update is called once per frame
@@ -21,14 +24,62 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
     void FixedUpdate()
-    {
+    {   
+        Vector3 forward = cam.transform.TransformDirection(Vector3.forward);
+        forward.y = 0;
+        forward = forward.normalized;
+        Vector3 right = new Vector3(forward.z, 0, -forward.x);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        moveDirection = (h * right + v * forward);
+        moveDirection *= speed;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.position += moveDirection * Time.deltaTime;
 
-        transform.position += (movement * speed);
+        if (h!=0 || v!=0)
+        {
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, 0.3f);
+        }
+
+        ////////   Basic  //////// 
+
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //transform.position += (movement * speed * 0.01f);
+
+        ////////   End Basic  //////// 
+
+
+        ////////   Moving where camera is looking   //////// 
+
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        ////transform.position += (movement * speed);
+
+        //Vector3 CameraToPlayer = transform.position - cam.transform.position;
+        //CameraToPlayer.y = 0.0f;
+        //Vector3 CameraToPlayerNorm = Vector3.Normalize(CameraToPlayer);
+        //Debug.Log(CameraToPlayerNorm);
+
+        //transform.position += (CameraToPlayerNorm * 0.01f) * moveVertical * speed;
+
+        //if (moveVertical > 0)
+        //{
+        //    transform.forward = CameraToPlayer;
+        //}
+        //else if (moveVertical < 0)
+        //{
+        //    transform.forward = -CameraToPlayer;
+        //}
+
+        ////////   End Moving where camera is looking   //////// 
 
     }
 }
---
+++ ProjectRogue/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public Transform target;
+    public float distance = 10.0f;
+    public float sensitivityX = 5.0f;
+    public float sensitivityY = 2.0f;
+
+    private Transform camTransform;
+    private Camera cam;
+
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 70.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+
+
+    private void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X") * sensitivityX;
+        currentY -= Input.GetAxis("Mouse Y") * sensitivityY;
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY + 10.0f, currentX, 0);
+        camTransform.position = target.position + rotation * dir;
+        camTransform.LookAt(target.position);
+    }
+}
