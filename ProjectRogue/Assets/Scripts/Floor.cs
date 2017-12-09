2dc767e14510b74793c63cf2809eea86a7064df3
Unknown <mitchell.jc@hotmail.com>
2017-12-09 16:28:35 +1300

    Add floor manager and bomb prefab, bomb spawning

---
+++ ProjectRogue/Assets/Scripts/Floor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	private bool hasDropped;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hasDropped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool HasFloorDropped()
+	{
+		return hasDropped;
+	}
+}
---
+++ ProjectRogue/Assets/Scripts/FloorManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour {
+
+	public GameObject Bomb;
+	private Floor[] floorArray;
+	private float bombTimer;
+	public float bombSpawnTime;
+	private float dropHeight;
+
+	// Use this for initialization
+	void Start ()
+	{
+		floorArray = GetComponentsInChildren<Floor>();
+		dropHeight = 20.0f;
+	}
+	
+	void FixedUpdate ()
+	{
+		bombTimer += Time.deltaTime;
+
+		if (bombTimer > bombSpawnTime)
+		{
+			SpawnBomb();
+			bombTimer = 0.0f;
+		}
+	}
+
+	void SpawnBomb()
+	{
+		int random = Random.Range(0, floorArray.Length);
+
+		if (!floorArray[random].HasFloorDropped())
+		{
+			Vector3 spawnLocation = floorArray [random].transform.position + new Vector3 (0, dropHeight, 0);
+			Instantiate (Bomb, spawnLocation, Quaternion.identity); 
+
+		}
+	}  
+}
