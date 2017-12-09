6d8b6e0124c009185b742a59f973666fdd85410b
Unknown <mitchell.jc@hotmail.com>
2017-12-09 13:26:43 +1300

    Add item manager, add collection bar and interactions

--- ProjectRogue/Assets/Scripts/Item.cs
+++ ProjectRogue/Assets/Scripts/Item.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour {
 
+	private Image collectionBar;
+	public float collectionValue;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		collectionBar = FindObjectOfType<ItemManager> ().GetCollectionBar ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+	{
 		
+	}
+
+	private void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.GetComponent<PlayerInteraction>())
+		{
+			Destroy (this);
+			collectionBar.fillAmount += 1.0f / collectionValue;
+		}
 	}
 }
---
+++ ProjectRogue/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemManager : MonoBehaviour {
+
+	public Image collectionBar;
+	private float collectionBarReduction;
+
+	// Use this for initialization
+	void Start () 
+	{
+		collectionBarReduction = 2000.0f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		collectionBar.fillAmount -= 1.0f / collectionBarReduction;
+	}
+
+	public Image GetCollectionBar()
+	{
+		return collectionBar;
+	}
+}
