e8aae4844ce47d80c80eb9b8431f35c7e113a0fa
mitchellcurrie <mitchell.jc@hotmail.com>
2018-03-25 22:50:17 +1300

    Make Game Manager singleton, work on text

--- ProjectRogue/Assets/Scripts/GameManager.cs
+++ ProjectRogue/Assets/Scripts/GameManager.cs
@@ -2,33 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour {
 
-	public TextMeshProUGUI gameTimerText;
-	private float gameTimer;
-	private bool gameStarted;
+    public static GameManager GMinstance = null;
+    private float gameTimer;
+	private bool gamePlaying;
+    private enum GameState { MENU, GAME, GAMEOVER };
+    private GameState CurrentState;
+    private TextManager TM;
 
-	// Use this for initialization
-	void Start () 
+    void Awake ()
+    {
+        if (GMinstance != null)
+        {
+            Destroy(gameObject);
+        }         
+        else
+        {
+            GMinstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    // Use this for initialization
+    void Start () 
 	{
-		gameStarted = true;
-	}
+        CurrentState = GameState.GAME;
+        TM = FindObjectOfType<TextManager>();
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		if (gameStarted) 
+		if (CurrentState == GameState.GAME) 
 		{
-			gameTimer += Time.deltaTime;
+            Debug.Log("In Game Part");
+            gameTimer += Time.deltaTime;
 			int intTimer = (int)gameTimer;
-			gameTimerText.SetText("Timer: " + intTimer.ToString());
+            TM.SetTimerText("Timer: " + intTimer.ToString());
 		}
-	}
+
+        else if (CurrentState == GameState.GAMEOVER)
+        {
+            Debug.Log("In GameOver Part");
+            int intTimer = (int)gameTimer;
+            TM.SetTimerText("You survived for " + intTimer.ToString() + " seconds!");          
+        }
+    }
 
     public void GameOver()
-    {
+    {        
         Debug.Log("Game Over");
+        CurrentState = GameState.GAMEOVER;
+        SceneManager.LoadScene("GameOver");
+        TM = FindObjectOfType<TextManager>();
     }
 }
---
+++ ProjectRogue/Assets/Scripts/TextManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextManager : MonoBehaviour {
+
+    public TextMeshProUGUI timerText;
+
+    public string GetTimerText()
+    {
+        return timerText.text;
+    }
+
+    public void SetTimerText(string _text)
+    {
+        timerText.text = _text;
+    }
+}
