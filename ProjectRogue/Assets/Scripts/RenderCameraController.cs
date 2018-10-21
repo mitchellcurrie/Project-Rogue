d5d683f3b69cce8cc3570c99655885efe9612c82
Mitchell Currie <mitchell.jc@hotmail.com>
2018-10-21 18:02:13 +0100

    Add big screen rendering texture of new rotating camera

---
+++ ProjectRogue/Assets/Scripts/RenderCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderCameraController : MonoBehaviour {
+
+	// Use this for initialization
+	private Vector3 startingPoint;
+	private const float moveSpeed = 10.0f;
+	void Start () {
+		
+		startingPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		transform.RotateAround(Vector3.zero,new Vector3(0,1,0), moveSpeed * Time.deltaTime);	
+	}
+}
